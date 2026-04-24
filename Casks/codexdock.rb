923bf79361fd9218b04b9024b5102c311da8ade1
bee <beelan@yeah.net>
2026-04-24 15:48:19 +0800

    fix: use version interpolation in cask

--- Casks/codexdock.rb
+++ Casks/codexdock.rb
@@ -5,7 +5,7 @@ cask "codexdock" do
   version "0.3.7"
   sha256 "8290e2fb7955890ef1660d38cc62508132bd19c5c7ba21d259ef76a0e37a3504"
 
-  url "https://github.com/bee1an/CodexDock/releases/download/v0.3.7/codexdock-0.3.7.dmg"
+  url "https://github.com/bee1an/CodexDock/releases/download/v#{version}/codexdock-#{version}.dmg"
   name "CodexDock"
   desc "Desktop account manager for Codex sessions"
   homepage "https://github.com/bee1an/CodexDock"
